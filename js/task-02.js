643c5634adcb3153ac56bda68223d23b18896ea0
Lyst67 <p_listopad@ukr.net>
2023-06-29 20:53:54 +0300

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ingr = document.querySelector("#ingredients");
+
+const ourIngredients = ingredients.map((ingredient) => {
+  const elm = document.createElement("li");
+  elm.textContent = ingredient;
+  elm.classList.add("item");
+  ingr.append(elm);
+});
