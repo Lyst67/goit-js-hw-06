a6522df3aa85e0fe7060f8fb22b2bb669dc04824
Lyst67 <p_listopad@ukr.net>
2023-07-01 13:08:57 +0300

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  const { email, password } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    alert("Please fill in all the fields!");
+  } else {
+    let formInfo = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(formInfo);
+  }
+  event.currentTarget.reset();
+}
