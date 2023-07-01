dd6962c60021318ec6c6de02762c0f96054f20eb
Lyst67 <p_listopad@ukr.net>
2023-07-01 13:36:37 +0300

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const bodyEl = document.querySelector("body");
+const spanEl = document.querySelector(".color");
+const changeBtnEl = document.querySelector(".change-color");
+
+changeBtnEl.addEventListener("click", onChangeColor);
+
+function onChangeColor(event) {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = bodyEl.style.backgroundColor;
+}
